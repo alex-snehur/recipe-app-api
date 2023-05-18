4673a9f665fe0a69aed2ab66f47a1b3d3bdda19d
alex-snehur <alexander.snehur@digitalinspirationlabs.io>
2023-05-18 21:25:57 +0200

    add test file

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+
+def add(x, y):
+    return x + y
+
+
+
+
+
+
+
+
+
+
---
+++ app/app/test.py
@@ -0,0 +1,17 @@
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        res = calc.add(2,2)
+        self.assertEqual(res, 4)
+
+
+
+
+
+
+
+
+
+        
