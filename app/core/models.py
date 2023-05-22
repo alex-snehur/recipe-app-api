81375ccafabf1f1171600efb6bc04970b0b51ab4
alex-snehur <alexander.snehur@digitalinspirationlabs.io>
2023-05-22 17:49:57 +0200

    add unit tests

--- app/core/models.py
+++ app/core/models.py
@@ -10,7 +10,14 @@ class UserManager(BaseUserManager):
             raise ValueError('User nust have an email adress')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(self._db)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self.db)
         return user
  
 class User(AbstractBaseUser, PermissionsMixin):
