8f775c21ba899135d7fbcb06abf73f5c11e84d14
alex-snehur <alexander.snehur@digitalinspirationlabs.io>
2023-06-01 13:26:19 +0200

    fix bug

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from core.models import Recipe
 
-class RecipeSerializer(serializers.ModelField):
+class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
