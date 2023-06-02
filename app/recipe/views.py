f6f1a3832c39739a1343735305676c49b60f040e
alex-snehur <alexander.snehur@digitalinspirationlabs.io>
2023-06-02 13:22:43 +0200

    add update tag api

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -29,6 +29,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return serializer.save(user=self.request.user)
 
 class TagViewSet(
+    mixins.UpdateModelMixin,
     mixins.ListModelMixin, 
     viewsets.GenericViewSet):
     serializer_class = serializers.TagSerializer
