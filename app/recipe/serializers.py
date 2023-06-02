86b5c8ba1a44e5bf6f54bbd930e668fbf5b153b2
alex-snehur <alexander.snehur@digitalinspirationlabs.io>
2023-06-02 13:22:07 +0200

    add tag api

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+)
+
 
 class RecipeSerializer(serializers.ModelSerializer):
 
@@ -9,8 +13,16 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
 
+
 class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
 
+
+class TagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
