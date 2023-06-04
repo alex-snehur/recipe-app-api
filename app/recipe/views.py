080a0dd33552317021b79b4bb9b617dc731b3361
alex-snehur <alexander.snehur@digitalinspirationlabs.io>
2023-06-04 14:14:03 +0200

    refactor code

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -107,11 +107,9 @@ class BaseRecipeAttrViewSet(
 
     def get_queryset(self):
         assigned_only = bool(int(self.request.query_params.get('assigned_only', 0)))
-
         queryset = self.queryset
         if assigned_only:
             queryset = queryset.filter(recipe__isnull=False)
-
         return queryset.filter(user=self.request.user).order_by('-name').distinct()
 
 
